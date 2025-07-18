94ac740d36f646d6122dc00418e47d63f8fea07b
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2025-07-18 17:11:39 -0400

    Ajust

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlaceController;
-
 
 // Rotas públicas (sem autenticação)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/teste', function () {
+    return response()->json(['message' => 'API funcionando!']);
+});
+
+
 // Rotas protegidas (precisam do token Sanctum)
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-});
 
-Route::middleware('auth:sanctum')->group(function () {
     Route::resource('places', PlaceController::class);
 });
